78443cabb1b6c9f9130857410b3ef078f8cb5fa7
HigorLoren <higor.lorenzon@gmail.com>
2019-06-09 00:04:47 -0300

    :art: Better Outside Click Listener

--- src/components/UI/FloatMenu/FloatMenu.js
+++ src/components/UI/FloatMenu/FloatMenu.js
@@ -1,35 +1,30 @@
-import React, { memo, useEffect } from "react";
+import React, { memo } from "react";
+import ClickedOutside from "../../../hoc/ClickedOutside/ClickedOutside";
 import { Fade } from "react-reveal";
 import classes from "./FloatMenu.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function FloatMenu(props) {
-  document.addEventListener("click", props.deleteMe);
-
-  useEffect(() => {
-    return () => {
-      document.removeEventListener("click", props.deleteMe);
-    };
-  }, [props.deleteMe]);
-
   return (
-    <Fade duration={250}>
-      <ul className={classes.FloatDropdownMenu} style={props.customStyle}>
-        {props.buttons.map((button, id) => (
-          <li key={id}>
-            <button
-              onClick={() => {
-                props.deleteMe();
-                button.onClickFunction();
-              }}
-            >
-              {button.icon ? <FontAwesomeIcon icon={button.icon} className="pr2" /> : null}
-              {button.text}
-            </button>
-          </li>
-        ))}
-      </ul>
-    </Fade>
+    <ClickedOutside onClickedOutside={props.deleteMe}>
+      <Fade duration={250}>
+        <ul className={classes.FloatDropdownMenu} style={props.customStyle}>
+          {props.buttons.map((button, id) => (
+            <li key={id}>
+              <button
+                onClick={() => {
+                  props.deleteMe();
+                  button.onClickFunction();
+                }}
+              >
+                {button.icon ? <FontAwesomeIcon icon={button.icon} className="pr2" /> : null}
+                {button.text}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </Fade>
+    </ClickedOutside>
   );
 }
 
---
+++ src/hoc/ClickedOutside/ClickedOutside.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+
+export default class ClickedOutside extends Component {
+  componentDidMount() {
+    document.addEventListener("mousedown", this.handleClickOutside);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("mousedown", this.handleClickOutside);
+  }
+
+  handleClickOutside = event => {
+    // IF exists the Ref of the wrapped component AND his dom children doesnt have the clicked component
+    if (this.wrapperRef && !this.wrapperRef.contains(event.target)) {
+      this.props.onClickedOutside();
+    }
+  };
+
+  render() {
+    // In this piece of code I'm trying to get to the first not functional component
+    // Because it wouldn't work if use a functional component (like <Fade/> from react-reveal)
+    let firstNotFunctionalComponent = this.props.children;
+    while (typeof firstNotFunctionalComponent.type === "function") {
+      firstNotFunctionalComponent = firstNotFunctionalComponent.props.children;
+    }
+
+    // Here I'm cloning the element because I have to pass a new prop, the "reference"
+    const children = React.cloneElement(firstNotFunctionalComponent, {
+      ref: node => {
+        this.wrapperRef = node;
+      },
+      // Keeping all the old props with the new element
+      ...firstNotFunctionalComponent.props
+    });
+
+    return <React.Fragment>{children}</React.Fragment>;
+  }
+}
