4e92b4996998510bcbc628126df9d0f61d1d2954
HigorLoren <higor.lorenzon@gmail.com>
2020-07-29 16:27:21 -0300

    Updates

---
+++ precache-manifest.df46e6c9b870d6de08f07b6b884503b8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f6ccc7f4c50b834cb74512e6ac52905",
+    "url": "/donko/index.html"
+  },
+  {
+    "revision": "03ca437d1d06526cd4fe",
+    "url": "/donko/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "af1aa72cdd1cdea8f93a",
+    "url": "/donko/static/css/main.d8ae9297.chunk.css"
+  },
+  {
+    "revision": "03ca437d1d06526cd4fe",
+    "url": "/donko/static/js/2.3049d38a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/donko/static/js/2.3049d38a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af1aa72cdd1cdea8f93a",
+    "url": "/donko/static/js/main.0f14ca9b.chunk.js"
+  },
+  {
+    "revision": "06ce7c8d9c005eb43575",
+    "url": "/donko/static/js/runtime-main.693e1d72.js"
+  },
+  {
+    "revision": "b91ff448b393207edc615681d6fde31a",
+    "url": "/donko/static/media/board.b91ff448.svg"
+  },
+  {
+    "revision": "bfb5debb2c4cf0a9c66aedd19e4aa6fd",
+    "url": "/donko/static/media/cancel.bfb5debb.svg"
+  },
+  {
+    "revision": "5a1bedfa8fb2733ada8bdd8a6955a683",
+    "url": "/donko/static/media/donko-logo.5a1bedfa.png"
+  },
+  {
+    "revision": "f5b548c89e785b005c2df98ce8d29e9a",
+    "url": "/donko/static/media/search.f5b548c8.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/donko/precache-manifest.df46e6c9b870d6de08f07b6b884503b8.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/donko/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
