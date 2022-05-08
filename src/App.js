f8472373bc8465997b31ec64d2d432993dc38f50
HigorLoren <higor.lorenzon@gmail.com>
2022-05-07 21:12:13 -0300

    ♻️ Move the fontAwesome to a separeted file

--- src/App.js
+++ src/App.js
@@ -1,38 +1,9 @@
 import React from 'react';
 import 'tachyons';
 
-import {
-  faCog,
-  faEllipsisV,
-  faPalette,
-  faPenSquare,
-  faPlus,
-  faPlusCircle,
-  faSignOutAlt,
-  faTimes,
-  faTimesCircle,
-  faTrashAlt,
-  faUserCog
-} from '@fortawesome/free-solid-svg-icons';
-
-import { library } from '@fortawesome/fontawesome-svg-core';
-
 import Auth from './auth';
 import AppRoutes from './routes';
-
-library.add(
-  faEllipsisV,
-  faPlus,
-  faPlusCircle,
-  faTimesCircle,
-  faPalette,
-  faPenSquare,
-  faTrashAlt,
-  faTimes,
-  faUserCog,
-  faSignOutAlt,
-  faCog
-);
+import './fontAwesome';
 
 if (Auth.userHasToken() && !Auth.isUserAuthenticated()) {
   Auth.authenticateUser(Auth.getLocalToken());
---
+++ src/fontAwesome.js
@@ -0,0 +1,29 @@
+import {
+  faCog,
+  faEllipsisV,
+  faPalette,
+  faPenSquare,
+  faPlus,
+  faPlusCircle,
+  faSignOutAlt,
+  faTimes,
+  faTimesCircle,
+  faTrashAlt,
+  faUserCog
+} from '@fortawesome/free-solid-svg-icons';
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+library.add(
+  faEllipsisV,
+  faPlus,
+  faPlusCircle,
+  faTimesCircle,
+  faPalette,
+  faPenSquare,
+  faTrashAlt,
+  faTimes,
+  faUserCog,
+  faSignOutAlt,
+  faCog
+);
